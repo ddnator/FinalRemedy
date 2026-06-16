fbb28844de5b2ff2aed390dea9ff4a22524854ad
ddnator <wangdarren3@gmail.com>
2026-06-16 09:50:13 +0200

    removed start resources

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,8 +2,6 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png'),
-    Shark: new ImageSource('images/shark.png')
 }
 
 
