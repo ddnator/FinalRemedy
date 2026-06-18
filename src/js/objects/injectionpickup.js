79e7e32484502fc1b8577c6edff556fcf677a179
Jayden <jaydenvlekke06@gmail.com>
2026-06-18 12:11:05 +0200

    Files added

---
+++ src/js/objects/injectionpickup.js
@@ -0,0 +1,43 @@
+import { Actor, Engine, Vector, CollisionType, DegreeOfFreedom, Scene, Keys } from "excalibur"
+import { Resources, ResourceLoader } from '../resources.js'
+import { Player } from "./player.js"
+import { Zombie } from "./zombie.js"
+
+export class Injection extends Actor {
+
+
+
+    constructor() {
+        super({
+            width: Resources.Injection.width / 3,
+            height: Resources.Injection.height / 3
+        })
+
+        this.body.collisionType = CollisionType.Passive
+        this.body.limitDegreeOfFreedom.push(DegreeOfFreedom.Rotation)
+
+
+        this.player = player
+
+
+    }
+
+    onInitialize(engine) {
+        const sprite = Resources.Injection.toSprite()
+        sprite.scale = new Vector(0.5, 0.5)
+        this.graphics.use(sprite)
+
+        // this.pos = new Vector(engine.drawWidth / 2, engine.drawHeight / 1.25)
+
+
+    }
+
+
+
+
+    onPostUpdate(engine) {
+
+    }
+
+
+}
