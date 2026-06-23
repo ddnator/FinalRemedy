fcc68625b5bb6eea8d8911e266533f985b78da45
Miya <chuyyanaka@gmail.com>
2026-06-23 14:40:49 +0200

    end, not freeze

--- src/js/objects/player.js
+++ src/js/objects/player.js
@@ -306,21 +306,14 @@ export class Player extends Actor {
         });
 
 
-        this.shootAnim = Animation.fromSpriteSheet(playerShootSheet, range(0, 5), frameSpeed, AnimationStrategy.Freeze);
+        this.shootAnim = Animation.fromSpriteSheet(playerShootSheet, range(0, 5), frameSpeed, AnimationStrategy.End);
         this.shootAnim.scale = new Vector(8, 8)
 
 
         this.graphics.add('shoot', this.shootAnim)
 
-
-
-
-
-
-
-
-
     }
+
     onCollisionStart(event, other) {
         if (other.owner instanceof Floor) {
             this.grounded = true
