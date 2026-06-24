980aa26fbd19065ef9a0b7e3aca6b9025097efe7
Jayden <jaydenvlekke06@gmail.com>
2026-06-24 13:07:15 +0200

    Small fixes

--- src/js/objects/quest.js
+++ src/js/objects/quest.js
@@ -114,7 +114,7 @@ export class Quest extends ScreenElement {
                 this.currentQuest = 'Keyfinder'
                 this.player.stuck = false
 
-                const key1 = new KeyPickup(4800, 941, 'key1')
+                const key1 = new KeyPickup(5000, 941, 'key1')
 
                 this.scene.add(key1)
 
