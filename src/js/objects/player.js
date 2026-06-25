93ae73f6452cca2e6fb07cee22059bb0b5800c9a
Miya <chuyyanaka@gmail.com>
2026-06-25 15:00:42 +0200

    walk added

--- src/js/objects/player.js
+++ src/js/objects/player.js
@@ -142,7 +142,7 @@ export class Player extends Actor {
         }
 
         if (xAccel !== 0) {
-            if (this.vel.x < 5000 && this.vel.x > -5000) {
+            if (this.vel.x < 2000 && this.vel.x > -2000) {
                 this.body.applyLinearImpulse(new Vector(xAccel * delta, 0));
             }
         }
