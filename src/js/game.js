7fb58186b34dea3931c63d55b7229b67f6cad2dd
ddnator <wangdarren3@gmail.com>
2026-06-29 10:32:22 +0200

    fixed player crouch bug

--- src/js/game.js
+++ src/js/game.js
@@ -40,7 +40,7 @@ export class Game extends Engine {
         this.add('scenethree', SceneThree)
         this.add('scenefour', Scenefour)
 
-        this.goToScene('sceneone')
+        this.goToScene('scenetwo')
 
         this.player = new Player(-7800, 800)
         this.ui = new UI()
